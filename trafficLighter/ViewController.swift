2db635851255121cf85c54dd587dc9036cc4f4c5
clubix <kirillsamarchenko@gmail.com>
2021-12-08 17:21:07 +0300

    Initial Commit

---
+++ trafficLighter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trafficLighter
+//
+//  Created by Кирилл Самарченко on 08.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
